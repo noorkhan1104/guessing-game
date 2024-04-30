a156f32d89e4e7380a88bd1934d527f75bed1887
noorkhan1104 <164567175+noorkhan1104@users.noreply.github.com>
2024-04-30 09:39:47 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const computerNumber = Math.floor(Math.random() * 10 + 1);
+const result = await inquirer.prompt([
+    {
+        name: "guessinggame",
+        type: "number",
+        message: "please guess a number between 1-10",
+    },
+]);
+if (result.guessinggame === computerNumber) {
+    console.log("congratulation ypu guess a right number");
+}
+else {
+    console.log("you guess the wrong number");
+}
